e4831c67f86608871c10dcc6909d42ab410ec866
Masoud Ramezani <masoudhra@gmail.com>
2024-01-08 15:28:06 +1300

    let and const

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello typescript");
+let myVar = "this is first value";
+const username = "Masoud Ramezani"
+
+console.log(myVar);
+myVar = "this is myVar value"
+console.log(myVar)
+myVar = "30"
+console.log(myVar)
