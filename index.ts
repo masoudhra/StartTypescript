1154e6454e17ebb84fb88c9e5e3376b9b33404bd
Masoud Ramezani <masoudhra@gmail.com>
2024-01-08 16:03:41 +1300

    inference type

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
-let BlogTitle : string = "this is first blog about typescript"
+let BlogTitle: string = "this is first blog about typescript";
 console.log(typeof BlogTitle);
 let blogLikesCount = 90;
 console.log(typeof blogLikesCount);
 let isActive = true;
 console.log(typeof isActive);
 
-let user : (typeof BlogTitle)
+let user: typeof BlogTitle;
 
-user = "Masoud"
-let str : string = `my blog has ${blogLikesCount} likes`
-console.log(str)
+user = "Masoud";
+let str: string = `my blog has ${blogLikesCount} likes`;
+console.log(str);
+
+// type inference
+let var1 = "string kdkdkd";
+let var2 = 25;
+let var3 = { key: "value" };
+
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
