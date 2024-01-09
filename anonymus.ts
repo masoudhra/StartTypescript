e65e5d0988e19097724b743ddd8dd2db4bde3e20
Masoud Ramezani <masoudhra@gmail.com>
2024-01-09 16:36:22 +1300

    [200~

---
+++ anonymus.ts
@@ -0,0 +1,26 @@
+// Anonymus Types
+let products: Array<{
+  title: string;
+  img: string;
+  price: number;
+  discount: number | undefined;
+  count: number;
+}>;
+let payment: { productAmount: number; geteway: string };
+let basket: {products: object, payment: object}
+basket = {
+    products: [
+        {
+            title: "Iphone 13 pro max",
+            img: "https://foolan.png",
+            price: 52000000,
+            discount: 5,
+            count: 500
+        }
+    ], 
+    payment: {
+        productAmount: 24700000000,
+        geteway: "zarrinpal"
+
+    }
+}
