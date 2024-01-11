5a6cd270c95b75b1bc607bf3fd1af1961557cf05
Masoud Ramezani <masoudhra@gmail.com>
2024-01-11 14:32:44 +1300

    ers barie proto type ha

---
+++ casting.ts
@@ -0,0 +1,7 @@
+let value1: any = "string";
+// let value2 = <number>value1;
+let value2 = value1 as number 
+console.log(typeof value2, value2)
+
+//mitoonim ers bari konim prototype ro 
+//asertion - explicit
